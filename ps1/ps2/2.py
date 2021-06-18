410c45adbcac5e4c038ac7c720ade5817f6975e9
Adam Ralph <adamralph@posteo.net>
2021-06-18 12:19:00 +1000

    added ps1 and ps2

---
+++ ps1/ps2/2.py
@@ -0,0 +1,29 @@
+balance = 3329
+annualInterestRate = 0.2
+
+# month = 0
+# while month < 12:
+    # minMonthlyPayment = balance * monthlyPaymentRate
+    # unpaidMonthlyBalance = balance - minMonthlyPayment
+    # newBalance = unpaidMonthlyBalance + monthlyInterestRate * unpaidMonthlyBalance
+    # balance = newBalance
+    # month += 1
+month = 0
+
+monthlyInterestRate = annualInterestRate/12.0
+monthlyPayment = 10
+increment = 1
+
+while month < 12:
+    unpaidMonthlyBalance = balance - monthlyPayment
+    monthlyBalance = unpaidMonthlyBalance + monthlyInterestRate * unpaidMonthlyBalance
+    balance = monthlyBalance
+    print(balance)
+    month += 1
+    if balance > 0:
+        monthlyPayment += increment
+    else:
+        break
+
+print(balance)
+print('Lowest Payment: {}'.format(monthlyPayment))
