28917e2b61a1b67479f5f8dfeb5b3ed305297ec9
Adam Ralph <adamralph@posteo.net>
2021-07-05 11:59:29 +1000

    need to add checks and 50 points for using all letters

---
+++ ps4/ps4_sketches.py
@@ -0,0 +1,23 @@
+VOWELS = 'aeiou'
+CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
+HAND_SIZE = 7
+n = HAND_SIZE
+
+SCRABBLE_LETTER_VALUES = {
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+}
+wordValues = SCRABBLE_LETTER_VALUES
+
+def getWordScore(word, n):
+    score = [] 
+    for letter in word:
+        score.append(wordValues[letter])        
+
+    print(score)
+    total = sum(score) * len(score)
+    print(total)
+
+word = 'gersdet'
+getWordScore(word, n)
+
+# print(wordValues['a'])
